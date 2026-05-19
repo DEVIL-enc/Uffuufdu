b74b686a81aeeb19082ec60606fbdf1fca7610dc
DEVIL-enc <moahmedjujuj@gmail.com>
2026-05-20 00:44:40 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -95,13 +95,13 @@ def bs(text):
 # ====================== CONFIG ======================
 API_ID = int(os.getenv(""))
 API_HASH = os.getenv("")
-BOT_TOKEN = os.getenv("8808007208:AAFfvIsUNbVf_bsXaBl34PAz6NL4ttL0STU", ")
-ADMIN_ID = json.loads(os.getenv("1707478010", "[]"))
-HIT_CHANNEL_ID = int(os.getenv("-5298202986", ""))
-JOIN_GROUP_ID = int(os.getenv("-5298202986", ""))
-JOIN_CHANNEL_ID = int(os.getenv("-1002144296199", ""))
-JOIN_GROUP_LINK = os.getenv("-5298202986", "")
-JOIN_CHANNEL_LINK = os.getenv("https://t.me/D3v1l_vip1", "")
+BOT_TOKEN = os.getenv("8808007208:AAFfvIsUNbVf_bsXaBl34PAz6NL4ttL0STU")
+ADMIN_ID = json.loads(os.getenv("1707478010"))
+HIT_CHANNEL_ID = int(os.getenv("-5298202986"))
+JOIN_GROUP_ID = int(os.getenv("-5298202986"))
+JOIN_CHANNEL_ID = int(os.getenv("-1002144296199"))
+JOIN_GROUP_LINK = os.getenv("-5298202986")
+JOIN_CHANNEL_LINK = os.getenv("https://t.me/D3v1l_vip1")
 FORCE_JOIN_IMAGES = [
     "",
     ""
